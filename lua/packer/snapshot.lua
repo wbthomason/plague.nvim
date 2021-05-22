26480f2249fa6b9a4af13b0f29f1d52f8af6c062
Zhi Fan <chenzhifan9@gmail.com>
2021-05-22 14:58:07 +0200

    Added filename parameter to PackerSnapshot command

--- lua/packer/snapshot.lua
+++ lua/packer/snapshot.lua
@@ -1,3 +1,8 @@
-local function snapshot()
-    
+local function do_snapshot(filename)
+    print(filename)
 end
+
+local function cfg(_config) config = _config end
+
+local snapshot = setmetatable({cfg = cfg}, {__call = do_snapshot})
+return snapshot
